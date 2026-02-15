0756f1df645e80d7e4e29798ca5a11fc7c916455
LailaArgueta24 <319128411@pcpuma.acatlan.unam.mx>
2026-02-15 13:11:07 -0600

    feat: agregando carpetas y archivos threads vs multiprocessing

---
+++ Repaso/map.py
@@ -0,0 +1,5 @@
+def cuadrado(n):
+	return n**2
+
+lista_cuadrada = list(map(cuadrado, range(10)))
+print(lista_cuadrada)
---
+++ Repaso/map_lambda.py
@@ -0,0 +1,2 @@
+lista_cuadrada = list(map(lambda x: x**2, range(10)))
+print(lista_cuadrada)
